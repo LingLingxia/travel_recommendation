b68525b00164d076795841ec5553a473f91a3c9f
LingLingxia <llxlansing@qq.com>
2024-07-18 15:12:38 -0700

    add click function

---
+++ travel_recommendation.js
@@ -0,0 +1,7 @@
+const searchButton = document.querySelector("#search");
+const searchInput = document.querySelector("#keyword");
+searchButton.addEventListener("click",()=>{
+    //console.log(searchInput.value);
+    const key = searchInput.value?.toLowerCase();
+    //fetch("/")
+})
